c84180ad3834a3af58edbf11edd0c51ef124e324
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2021-09-17 15:29:13 -0700

    finished PolyTreeNode 1

--- W3/D5/PolyTreeNode/lib/00_tree_node.rb
+++ W3/D5/PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class PolyTreeNode
 
   attr_reader :value, :parent, :children
@@ -9,11 +11,23 @@ class PolyTreeNode
   end
 
   def parent=(parent)
+    #debugger
+    if self.parent 
+      self.parent.children.delete(self)
+    end
     @parent = parent
-    parent.children << self if !parent
+    @parent.children << self if parent && !@parent.children.include?(self)
   end
 
   def add_child(child)
+   child.parent = self
+  end
 
+  def remove_child(child)
+    #if the parent node doesn't have that child, return an error
+    if !self.children.include?(child)
+      raise "Node is not a child"
+    end
+    child.parent = nil
   end
 end
