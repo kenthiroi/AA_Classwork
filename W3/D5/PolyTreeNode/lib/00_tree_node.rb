29b3e24aac6f494ecca32ec205fe9bbb1daf44a6
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-09-17 15:12:19 -0700

    Added initialize, parent= and add_child methods

--- W3/D5/PolyTreeNode/lib/00_tree_node.rb
+++ W3/D5/PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,19 @@
 class PolyTreeNode
 
+  attr_reader :value, :parent, :children
+
+  def initialize(value)
+    @children = []
+    @value = value
+    @parent = nil
+  end
+
+  def parent=(parent)
+    @parent = parent
+    parent.children << self if !parent
+  end
+
+  def add_child(child)
+
+  end
 end
