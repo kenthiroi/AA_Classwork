2197c3dff25e2fa14caa51080c722e0a262756e7
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-09-20 13:56:17 -0700

    Added #new_move_positions and finished ::valid_moves

--- W3/D5/KnightsTravails/knights_travails.rb
+++ W3/D5/KnightsTravails/knights_travails.rb
@@ -9,10 +9,31 @@ class KnightPathFinder
 
   def initialize(starting_pos)
     @starting_pos = starting_pos
+    @considered_positions = [starting_pos]
+  end
+
+  def new_move_positions(pos)
+    valid_moves = KnightPathFinder.valid_moves(pos)
+    valid_moves.each do |move|
+      if !@considered_positions.include?(move)
+        @considered_positions << move
+      end
+    end
+    @considered_positions
   end
 
   def self.valid_moves(pos)
     #knight at [0,0] has [1,2], [2,1], [-1, 2], [-2,1], [1,-2], [-2,1], [-2,-1], [-1,-2]
-    
+    knight_moves = [[1,2], [2,1], [-1, 2], [-2,1], [1,-2], [2,-1], [-2,-1], [-1,-2]]
+    valid_moves = []
+    knight_moves.each do |move|
+      move_row = move.first + pos.first
+      move_col = move.last + pos.last
+      if move_row <= 8 && move_row >= 0 && move_col <= 8 && move_col >= 0
+        valid_moves
+        valid_moves << [move_row, move_col]
+      end
+    end
+    valid_moves
   end
 end
