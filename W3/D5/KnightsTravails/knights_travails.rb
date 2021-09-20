6775eac11e21a391f4dd0a5080ff59d5a0f93aac
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2021-09-20 16:54:23 -0700

    working and waiting for code check

--- W3/D5/KnightsTravails/knights_travails.rb
+++ W3/D5/KnightsTravails/knights_travails.rb
@@ -2,14 +2,33 @@ require_relative "../PolyTreeNode/lib/00_tree_node.rb"
 require 'byebug'
 
 class KnightPathFinder
-
+  attr_reader :considered_positions
   def build_move_tree(root_node)
     #When do we stop building the tree?
-    root_node = PolyTreeNode.new(root_node)
-    
+    #root_node = PolyTreeNode.new(root_node)
     
     #build a queue of children
+    #new_move_pos(pos) will update @considered positions and return relevant ones
     #shovel @considered positions
+    #make all possible positions from the root_node and them as children to root node 
+    
+    all_pos = [root_node]
+    #if current nodes doesn't have new_move_pos, it means it has 
+    #no children, and we can move to the next node in all_pos
+    i = 0 
+    while all_pos.length < 64
+      #debugger if not all_pos[i]
+      possible_pos = new_move_positions(all_pos[i].value)
+      if !possible_pos.empty?
+        possible_pos.each do |move|
+          temp = PolyTreeNode.new(move)
+          temp.parent = all_pos[i]
+          all_pos << temp
+        end
+      end 
+      i += 1
+    end
+
 
     # possible_moves = KnightPathFinder.valid_moves(root_node.value)
     # queue = [root_node]
@@ -68,7 +87,7 @@ class KnightPathFinder
       #debugger if pos.value.class != [].class
       move_row = move.first + pos.first
       move_col = move.last + pos.last
-      if move_row <= 8 && move_row >= 0 && move_col <= 8 && move_col >= 0
+      if move_row <= 7 && move_row >= 0 && move_col <= 7 && move_col >= 0
         valid_moves
         valid_moves << [move_row, move_col]
       end
