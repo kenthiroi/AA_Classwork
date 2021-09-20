8134c96959087a045d3d88393e65d3fa36cd7912
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2021-09-20 13:40:21 -0700

    first go

---
+++ W3/D5/KnightsTravails/knights_travails.rb
@@ -0,0 +1,18 @@
+require_relative "../PolyTreeNode/lib/00_tree_node.rb"
+
+class KnightPathFinder
+  #x, y, 
+  def build_move_tree(root_node)
+
+  end
+
+
+  def initialize(starting_pos)
+    @starting_pos = starting_pos
+  end
+
+  def self.valid_moves(pos)
+    #knight at [0,0] has [1,2], [2,1], [-1, 2], [-2,1], [1,-2], [-2,1], [-2,-1], [-1,-2]
+    
+  end
+end
