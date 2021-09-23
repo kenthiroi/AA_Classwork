ccfba45fd678c9d5f66db116c4f294bcda317e5d
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-09-23 16:52:54 -0700

    completed #move method and tests

--- W4/D4/TDD Project/Towers_of_hanoi/lib/towers.rb
+++ W4/D4/TDD Project/Towers_of_hanoi/lib/towers.rb
@@ -8,5 +8,11 @@ class TowersOfHanoi
     @stack3 = [5, 4, 3, 2, 1]
   end
 
+  def move(stack1, stack2)
+    raise "Stack is empty" if stack1.empty?
+    raise "Larger than top piece in stack" if stack1[-1] > stack2[-1]
+    stack2.push(stack1.pop) 
+  end
+
 
 end
--- W4/D4/TDD Project/Towers_of_hanoi/spec/towers_spec.rb
+++ W4/D4/TDD Project/Towers_of_hanoi/spec/towers_spec.rb
@@ -12,11 +12,7 @@ describe "TowersOfHanoi" do
       it 'should create 3 stacks' do
         expect(tower.stack1).to eq([])
         expect(tower.stack2).to eq([])
-        expect(tower.stack3).to eq([])
-      end
-
-      it 'should create pieces' do
-        expect(tower.pieces).to eq([1, 2, 3, 4, 5])
+        expect(tower.stack3).to eq([5, 4, 3, 2, 1])
       end
       
     end
@@ -30,13 +26,31 @@ describe "TowersOfHanoi" do
     end
 
     it 'should move piece to different stack' do
-      expect{ tower.move(stack3, stack1) }.to change { tower.stack1 }.
+      expect{ tower.move(tower.stack3, tower.stack1) }.to change { tower.stack1 }.
         from( [5] ).
         to( [5, 4] )
     end
 
-    it 'should not pile disk smaller than itself' do
-      expect { tower.move(stack1, stack3) }.to raise_error()
+    it 'removes from old stack' do
+      expect{ tower.move(tower.stack3, tower.stack1) }.to change { tower.stack3 }.
+        from( [4] ).
+        to( [] )
+    end
+    context "error management" do 
+      before(:each) do
+        tower.stack1 = [5, 4]
+        tower.stack2 = [3, 2, 1]
+        tower.stack3 = []
+      end
+
+      it 'should not pull from empty stack' do
+        expect{ tower.move(tower.stack3, tower.stack1) }.to raise_error("Stack is empty")
+      end
+
+      it 'should not pile disk smaller than itself' do
+        expect { tower.move(tower.stack1, tower.stack2) }.to raise_error("Larger than top piece in stack")
+      end
+
     end
 
   end
