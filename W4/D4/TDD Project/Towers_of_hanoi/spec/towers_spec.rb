ad2a000a8d647f941e6ad6a2638c0a0218727cc3
Williamsliam23 <williams.liam23@gmail.com>
2021-09-23 18:33:25 -0500

    tower move method specs

--- W4/D4/TDD Project/Towers_of_hanoi/spec/towers_spec.rb
+++ W4/D4/TDD Project/Towers_of_hanoi/spec/towers_spec.rb
@@ -22,5 +22,23 @@ describe "TowersOfHanoi" do
     end
   end
 
+  describe '#move' do
+    before(:each) do
+      tower.stack1 = [5]
+      tower.stack2 = [3, 2, 1]
+      tower.stack3 = [4]
+    end
+
+    it 'should move piece to different stack' do
+      expect{ tower.move(stack3, stack1) }.to change { tower.stack1 }.
+        from( [5] ).
+        to( [5, 4] )
+    end
+
+    it 'should not pile disk smaller than itself' do
+      expect { tower.move(stack1, stack3) }.to raise_error()
+    end
+
+  end
 
 end
