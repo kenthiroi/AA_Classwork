7d59e18aa939a8d9be377f4f5dee1ed5781d5390
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-09-24 16:56:36 -0700

    Finished #anagram methods

--- W4/D5/anagrams.rb
+++ W4/D5/anagrams.rb
@@ -1,17 +1,9 @@
-# def first_anagram?(str1, str2)
-#   # chars_hash = Hash.new(0)
-#   # chars = str1.split
-#   # all_anagrams = []
-  
-#   # chars.each { |c| chars_hash[c] += 1 }
 
-# end
-
-# def first_anagram?(str1, str2)
-#   chars = str1.split('')
-#   all_words = chars.permutation.to_a
-#   all_words.include?(str2.split(''))
-# end
+def first_anagram?(str1, str2)
+  chars = str1.split('')
+  all_words = chars.permutation.to_a
+  all_words.include?(str2.split(''))
+end
 
 def second_anagram?(str1, str2)
   letters = str2.split('')
@@ -25,5 +17,23 @@ def second_anagram?(str1, str2)
   letters.empty?
 end
 
-p second_anagram?("gizmo", "sally")    #=> false
-p second_anagram?("elvis", "lives")    #=> true
+def third_anagram?(str1, str2)
+   test1 = str1.split('').sort
+   test2 = str2.split('').sort
+   test1 == test2
+end
+
+def fourth_anagram?(str1, str2)
+  chars_hash = Hash.new(0)
+  chars = str1.split('')
+  chars_hash2 = Hash.new(0)
+  chars2 = str2.split('')
+  
+  chars.each { |c| chars_hash[c] += 1 }
+  chars2.each { |c| chars_hash2[c] += 1 }
+
+  chars_hash == chars_hash2
+end
+
+p fourth_anagram?("gizmo", "sally")    #=> false
+p fourth_anagram?("elvis", "lives")    #=> true
