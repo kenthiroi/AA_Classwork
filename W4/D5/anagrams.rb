ce06271dd773f9c9209c37ab579c31dc47bf267b
unknown <mrkdawes8@gmail.com>
2021-09-24 19:21:06 -0400

    create anagram methods

--- W4/D5/anagrams.rb
+++ W4/D5/anagrams.rb
@@ -1,8 +1,29 @@
-def first_anagram?(str1, str2)
-  chars_hash = Hash.new(0)
-  chars = str1.split
-  all_anagrams = []
+# def first_anagram?(str1, str2)
+#   # chars_hash = Hash.new(0)
+#   # chars = str1.split
+#   # all_anagrams = []
   
-  chars.each { |c| chars_hash[c] += 1 }
+#   # chars.each { |c| chars_hash[c] += 1 }
 
+# end
+
+# def first_anagram?(str1, str2)
+#   chars = str1.split('')
+#   all_words = chars.permutation.to_a
+#   all_words.include?(str2.split(''))
+# end
+
+def second_anagram?(str1, str2)
+  letters = str2.split('')
+  str1.each_char do |char|
+    current_char = letters.index(char)
+    if current_char
+      letters.delete_at(current_char)
+    end
+    p letters
+  end
+  letters.empty?
 end
+
+p second_anagram?("gizmo", "sally")    #=> false
+p second_anagram?("elvis", "lives")    #=> true
