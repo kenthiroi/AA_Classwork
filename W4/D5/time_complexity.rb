bc7a93de462fcbe9574ce93ec2c8013403f386c7
unknown <mrkdawes8@gmail.com>
2021-09-24 17:26:19 -0400

    start time complex

--- W4/D5/time_complexity.rb
+++ W4/D5/time_complexity.rb
@@ -10,11 +10,35 @@
 #   end
 # end
 
-def my_min(list)
-  min = list.first
-  list.each { |el| min = el if el < min }
-  min
+# def my_min(list)
+#   min = list.first
+#   list.each { |el| min = el if el < min }
+#   min
+# end
+
+# def largest_contiguous_subsum(list)
+#   largest_sum = 0
+#   i = 0
+#   while i < list.length
+#     largest_sum = list[i] if list[i] > largest_sum
+#     j = i + 1
+#     while j < list.length
+#       sum = list[i..j].inject {|acc, n| acc+n }
+#       largest_sum = sum if sum > largest_sum
+#       j += 1
+#     end
+#     i += 1
+#   end
+#   largest_sum
+# end
+
+def largest_contiguous_subsum(list)
+  largest_sum = 0
+  list.each_with_index do |num, i|
+    current_sum = list[i]
+    
+  end
 end
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)
+list = [5, 3, -7]
+p largest_contiguous_subsum(list)
