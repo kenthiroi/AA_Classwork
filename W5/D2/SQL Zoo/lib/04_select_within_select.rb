7830b0013c11b1970b5393ff0fc0641aa2dd0176
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-09-28 16:35:55 -0700

    Working on select_within_select

--- W5/D2/SQL Zoo/lib/04_select_within_select.rb
+++ W5/D2/SQL Zoo/lib/04_select_within_select.rb
@@ -35,6 +35,19 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    countries
+  WHERE
+    population > (
+      SELECT
+       population
+      FROM
+        countries
+      WHERE
+        name = 'Russia'
+      )
   SQL
 end
 
@@ -42,6 +55,19 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    countries
+  WHERE
+    gdp/population > (
+      SELECT
+        gdp/population
+      FROM
+        countries
+      WHERE
+        name = 'United Kingdom'
+    ) AND continent = 'Europe'
   SQL
 end
 
@@ -49,6 +75,19 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+  SELECT
+    name, continent
+  FROM
+    countries
+  WHERE
+    continent IN (
+      SELECT
+        continent
+      FROM
+        countries
+      WHERE
+        name = 'Belize' OR name = 'Belgium'
+    )
   SQL
 end
 
