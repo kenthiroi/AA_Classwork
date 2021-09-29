e3faab47111c39335ac890feae476f6785950ebd
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-09-29 10:23:38 -0700

    created import_db.sql file

---
+++ W5/D3/import_db.sql
@@ -0,0 +1,27 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS questions_follow;
+CREATE TABLE users(
+  fname text NOT NULL,
+  lname text NOT NULL,
+  id integer PRIMARY KEY
+);
+
+
+CREATE TABLE questions(
+  title text NOT NULL,
+  body text NOT NULL,
+  author_id integer NOT NULL,
+  id integer PRIMARY KEY,
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE questions_follow(
+  id integer PRIMARY KEY,
+  user_id integer NOT NULL,
+  question_id integer NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
