bb0c98d06ec9a42ec6c4a3a6d3fd9e939912fbde
Emmett He <emmetthe26@gmail.com>
2021-09-29 10:39:21 -0700

    create table replies and question likes

--- W5/D3/import_db.sql
+++ W5/D3/import_db.sql
@@ -3,6 +3,8 @@ PRAGMA foreign_keys = ON;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS questions_follow;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_likes;
 CREATE TABLE users(
   fname text NOT NULL,
   lname text NOT NULL,
@@ -24,4 +26,35 @@ CREATE TABLE questions_follow(
   question_id integer NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (question_id) REFERENCES questions(id)
-);`
+);
+
+CREATE TABLE replies(
+  id integer PRIMARY KEY,
+  question_id integer NOT NULL,
+  user_id integer NOT NULL,
+);
+
+CREATE TABLE question_likes(
+
+);
+
+INSERT INTO 
+  users(fname, lname)
+
+VALUES
+  ('Kent', 'Hiroi'), 
+  ('Emmett', 'He');
+
+INSERT INTO
+  questions(title, body, author_id)
+
+VALUES 
+  ('Kent Question', 'Kent Kent Kent', 1),
+  ('Emmett Question', 'Emmett Emmett Emmett', 2);
+
+INSERT INTO 
+  questions_follow(user_id, question_id)
+
+VALUES
+  (1, 1),
+  (2, 2);
