1c99282fa7762f772b7e271bc7a07b611daeb0d5
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-10-06 15:03:51 -0700

    removed submodule

---
+++ W6/D3/first_routes/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/users/', to: 'users#index', as: 'get_all_users'
+  post '/users/', to: 'users#create', as: 'create_user'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get '/users/:id', to: 'users#show', as: 'show_user'
+  patch '/users/:id', to: 'users#update', as: 'patch_user'
+  put '/users/:id', to: 'users#update', as: 'put_user'
+  delete '/users/:id', to: 'users#destroy', as: 'delete_user'
+end
