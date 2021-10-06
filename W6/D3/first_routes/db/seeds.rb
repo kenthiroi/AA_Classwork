9a478def517f9ddf84484994ae56736d0c8453e0
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-10-06 16:41:55 -0700

    migrated models into db

--- W6/D3/first_routes/db/seeds.rb
+++ W6/D3/first_routes/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([{ name: 'Kent Hiroi', email: 'kento@kentobento.com' }, { name: 'Abraham Fong', email: 'abraham@abraham.com' }, { name: 'Bob Burgerman', email: 'bob@burgermeister.com'}])
