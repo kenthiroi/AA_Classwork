3d5e2ea0794c60f15b0da18da0c4c802cee632b1
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-10-06 17:31:01 -0700

    added error message for #create, created #show and #update

--- W6/D3/first_routes/app/controllers/users_controller.rb
+++ W6/D3/first_routes/app/controllers/users_controller.rb
@@ -8,8 +8,24 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(params.require(:user).permit(:name, :email))
-    user.save!
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find(params[:id])
     render json: user
+  end
+
+  def update
+    render json: User.update(user_params)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
