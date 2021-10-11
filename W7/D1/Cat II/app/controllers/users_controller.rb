7e190787941d5153502b26dfb769342e5ae5298d
Jan Daniel Fermin <jandanielfermin@gmail.com>
2021-10-11 16:04:50 -0700

    added forms for sessions and users

--- W7/D1/Cat II/app/controllers/users_controller.rb
+++ W7/D1/Cat II/app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
-  
+  def new
+    render :new
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      login(user)
+      redirect_to user_url(user)
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
