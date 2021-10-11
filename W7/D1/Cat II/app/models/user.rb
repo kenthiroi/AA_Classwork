6fdce5a14373c26c53f0a12cb09ad9b277687d2a
Jan Daniel Fermin <jandanielfermin@gmail.com>
2021-10-11 14:43:28 -0700

    finished user model

--- W7/D1/Cat II/app/models/user.rb
+++ W7/D1/Cat II/app/models/user.rb
@@ -10,12 +10,41 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  attr_reader :password
+
+  after_initialize :ensure_session_token
   validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
-  validates :password_digest, length: {minimum: 6}, allow_nil: true
+  validates :password, length: {minimum: 6}, allow_nil: true
 
   def reset_session_token!
     self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
   end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
+  def password=(password)
+    self.password_digest = BCrpyt::Password.create(password)
+    @password = password
+  end
+
+  def is_password?(password)
+    password_object = BCrpyt::Password.new(self.password_digest)
+    password_object.is_password?(password)
+  end
+
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(username: user_name)
+    if user && user.is_password?(password)
+      return user
+    else
+      nil
+    end
+  end
+
 
 end
