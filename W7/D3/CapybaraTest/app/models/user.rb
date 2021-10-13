c30fe59ab46cc9b67d1485aa78d1d9b8e7b399c9
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2021-10-13 19:06:46 -0400

    Add specs to User model

--- W7/D3/CapybaraTest/app/models/user.rb
+++ W7/D3/CapybaraTest/app/models/user.rb
@@ -1,5 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
   validates :username, :session_token, uniqueness: true, presence: true
   validates :password_digest, presence: true
+  
   
 end
--- W7/D3/CapybaraTest/spec/models/user_spec.rb
+++ W7/D3/CapybaraTest/spec/models/user_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
@@ -9,5 +20,20 @@ RSpec.describe User, type: :model do
   it { should validate_length_of(:password).is_at_least(6) }
   it { should have_many(:goals) }
 
-  
+  # feature "find by credentials" do 
+    
+  # end
+
+  feature "is_valid_password?" do
+    scenario "with a valid password" do
+      it "should return true" do 
+        expect(user.is_valid_password?('password')).to be true 
+      end
+    end 
+    scenario "with an invalid password" do
+      it "should return false" do 
+        expect(user.is_valid_password?('123456')).to be false
+      end
+    end 
+  end
 end
