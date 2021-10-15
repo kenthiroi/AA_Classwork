494b617260ab5f5ae3a1fd8c5273241ae84066ee
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-10-15 14:47:33 -0700

    Adds users and sessions controllers

--- W7/D5/RedditClone/app/controllers/sessions_controller.rb
+++ W7/D5/RedditClone/app/controllers/sessions_controller.rb
@@ -1,2 +1,31 @@
 class SessionsController < ApplicationController
+  # new, create login, destroy logout
+  before_action :require_logged_in, only: :destroy
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(user_params)
+    if @user.nil?
+      flash.now[:errors] = ["Invalid Credentials"]
+      render :new
+    else
+      log_in(@user)
+      redirect_to subs_url
+    end
+  end
+
+  def destroy
+    log_out
+    render :new
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
--- W7/D5/RedditClone/app/controllers/users_controller.rb
+++ W7/D5/RedditClone/app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
-  
+  # new, create Sign up 
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:username, :password))
+    if @user.save
+      log_in(@user)
+      redirect_to subs_url
+    else
+      flash.now[:errors] = ["Invalid Username or Password"]
+      render :new
+    end
+  end
+
 end
