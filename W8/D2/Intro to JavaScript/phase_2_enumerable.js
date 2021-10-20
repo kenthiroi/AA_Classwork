4f1e615d18d9a5070c157ca1dbb7a330c6bcf4ea
Grayson Poon <grayson.a.poon@gmail.com>
2021-10-19 17:30:50 -0700

    Work on phase 2

--- W8/D2/Intro to JavaScript/phase_2_enumerable.js
+++ W8/D2/Intro to JavaScript/phase_2_enumerable.js
@@ -1,5 +1,31 @@
-Array.prototype.myEach(callback) = function() {
+Array.prototype.myEach = function(callback) {
   for (i = 0; i < this.length; i++) {
-    this[i].callback();
+    callback(this[i]);
   }
+}
+
+// let testArr = [2,5,10];
+// let timesThree = function(n) {
+//     return n * 3;
+// }
+
+Array.prototype.myMap = function(callback) {
+  let newArr = [];
+
+  this.myEach(function(ele) {
+    newArr.push(callback(ele));
+  })
+
+  // newArr.myEach(callback)
+  
+  // for (i = 0; i < this.length; i++) {
+  //   newArr.push(callback(this[i]))
+  // }
+
+  return newArr;
+}
+
+let testArr = [5,20, 50];
+let divideByFive = function(n) {
+  return n / 5;
 }
