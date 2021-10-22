20cd10b6f95b335d6051ffa423a319df6c16b315
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-10-22 15:26:46 -0700

    finish curried sum [ar]

--- W8/D5/arguments Exercise Problems/argumentsExercise.js
+++ W8/D5/arguments Exercise Problems/argumentsExercise.js
@@ -32,30 +32,55 @@ class Dog {
   }
 }
 
-const markov = new Cat("Markov");
-const pavlov = new Dog("Pavlov");
+// const markov = new Cat("Markov");
+// const pavlov = new Dog("Pavlov");
 
-markov.says("meow", "Ned");
-// Markov says meow to Ned!
-// true
+// markov.says("meow", "Ned");
+// // Markov says meow to Ned!
+// // true
 
-// bind time args are "meow" and "Kush", no call time args
-markov.says.myBind(pavlov, "meow", "Kush")();
-// Pavlov says meow to Kush!
-// true
+// // bind time args are "meow" and "Kush", no call time args
+// markov.says.myBind(pavlov, "meow", "Kush")();
+// // Pavlov says meow to Kush!
+// // true
 
-// no bind time args (other than context), call time args are "meow" and "a tree"
-markov.says.myBind(pavlov)("meow", "a tree");
-// Pavlov says meow to a tree!
-// true
+// // no bind time args (other than context), call time args are "meow" and "a tree"
+// markov.says.myBind(pavlov)("meow", "a tree");
+// // Pavlov says meow to a tree!
+// // true
 
-// bind time arg is "meow", call time arg is "Markov"
-markov.says.myBind(pavlov, "meow")("Markov");
-// Pavlov says meow to Markov!
-// true
+// // bind time arg is "meow", call time arg is "Markov"
+// markov.says.myBind(pavlov, "meow")("Markov");
+// // Pavlov says meow to Markov!
+// // true
 
-// no bind time args (other than context), call time args are "meow" and "me"
-const notMarkovSays = markov.says.myBind(pavlov);
-notMarkovSays("meow", "me");
-// Pavlov says meow to me!
-// true
+// // no bind time args (other than context), call time args are "meow" and "me"
+// const notMarkovSays = markov.says.myBind(pavlov);
+// notMarkovSays("meow", "me");
+// // Pavlov says meow to me!
+// // true
+
+
+function curriedSum(numArgs) {
+  let numbers = [];
+
+  function _curriedSum(num) {
+    numbers.push(num);
+    if (numbers.length === numArgs) {
+      let sum = 0;
+      for (let i = 0; i < numArgs; i++) {
+        sum += numbers[i];
+      }
+      console.log(sum);
+      return sum;
+    } else {
+      return _curriedSum;
+    }
+  }
+
+  return _curriedSum;
+  
+}
+
+const newSum = curriedSum(4);
+newSum(5)(30)(20)(1); // => 56
